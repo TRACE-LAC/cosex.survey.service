e04332091786b50f1738152c1c164ae34730407e
GeraldineGomez <avocadoenterprise@gmail.com>
2024-03-06 10:44:48 -0500

    feat(security): created the security token filter

---
+++ src/main/java/com/monkeypox/survey/service/security/TokenFilter.java
@@ -0,0 +1,72 @@
+package com.monkeypox.survey.service.security;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.UnsupportedJwtException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Slf4j
+public class TokenFilter extends OncePerRequestFilter {
+
+    @Autowired
+    private SecurityUtils securityUtils;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+        
+        try {
+            if (securityUtils.checkJWTToken(request, response)) {
+				Claims claims = securityUtils.validateToken(request);
+				if (claims.get("authorities") != null) {
+					setUpSpringAuthentication(claims);
+				} else {
+					SecurityContextHolder.clearContext();
+				}
+			}else {
+				SecurityContextHolder.clearContext();
+			}
+			
+            filterChain.doFilter(request, response);
+
+		} catch (ExpiredJwtException | UnsupportedJwtException | MalformedJwtException e) {
+			response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+            e.printStackTrace();
+            log.error("Error en doFilter", e.getMessage());
+			((HttpServletResponse) response).sendError(HttpServletResponse.SC_FORBIDDEN, e.getMessage());
+			return;
+		}
+    }
+
+	/**
+	 * Authentication method in Spring flow
+	 * @param claims
+	 */
+	private void setUpSpringAuthentication(Claims claims) {
+		@SuppressWarnings("unchecked")
+		List<String> authorities = (List<String>) claims.get("authorities");
+
+		UsernamePasswordAuthenticationToken auth = new UsernamePasswordAuthenticationToken(claims.getSubject(), null,
+				authorities.stream().map(SimpleGrantedAuthority::new).collect(Collectors.toList()));
+		SecurityContextHolder.getContext().setAuthentication(auth);
+
+	}
+
+	
+    
+}
